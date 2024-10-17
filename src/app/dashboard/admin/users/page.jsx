5df9ec7cd564679000d8d113a33be1fb4cf13aa0
k3lly003 <ntirukelly@gmail.com>
2024-10-17 21:39:47 +0200

    @next/linux 2 @next/win 02

--- src/app/dashboard/admin/users/page.jsx
+++ src/app/dashboard/admin/users/page.jsx
@@ -1,27 +1,35 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import { IoIosAddCircleOutline } from "react-icons/io";
 import { TbPencil } from "react-icons/tb";
 import { RiDeleteBin6Line } from "react-icons/ri";
-import AddUserModal from './addUserMoal';
-import EditUserModal from './editUserModal';
-import DeleteUserModal from './deleteUserModal';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
+import AddUserModal from "./addUserMoal";
+import EditUserModal from "./editUserModal";
+import DeleteUserModal from "./deleteUserModal";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { HiOutlineSearch } from "react-icons/hi";
+import Image from "next/image";
 
 const UserCard = ({ name, email, jobTitle, onEdit, onDelete }) => {
   return (
     <Card className="w-full sm:w-[calc(33.333%-0.75rem)] shadow-sm relative">
       <div className="absolute top-2 right-2 flex flex-col space-y-2">
         <TbPencil className="text-[#0FA958] cursor-pointer" onClick={onEdit} />
-        <RiDeleteBin6Line className="text-[#FF0202] cursor-pointer" onClick={onDelete} />
+        <RiDeleteBin6Line
+          className="text-[#FF0202] cursor-pointer"
+          onClick={onDelete}
+        />
       </div>
       <CardContent className="flex items-center justify-center bg-[#EAEAEA] rounded-lg">
         <div className="flex flex-col items-center pt-6 gap-4">
-          <img src="/userAvatar.png" alt="User Avatar" className="w-12 h-12 rounded-full mb-2" />
+          <Image
+            src="/userAvatar.png"
+            alt="User Avatar"
+            className="w-12 h-12 rounded-full mb-2"
+          />
           <div className="text-center">
             <CardTitle className="text-lg font-normal">{name}</CardTitle>
             <p className="text-sm text-muted-foreground">{email}</p>
@@ -40,10 +48,10 @@ const ManageUsers = () => {
 
   return (
     <>
-      <div className='p-8'>
+      <div className="p-8">
         <div>
           <div>
-            <p className='text-[18px] items-center flex justify-between'>
+            <p className="text-[18px] items-center flex justify-between">
               Manage User
               <span>
                 <div className="relative w-80 ">
@@ -56,11 +64,8 @@ const ManageUsers = () => {
                   <HiOutlineSearch className="absolute left-3 top-1/2 transform -translate-y-1/2" />
                 </div>
               </span>
-              <span
-                className='flex'
-                onClick={() => setIsAddModalOpen(true)}
-              >
-                <Button className="flex gap-2 text-white" >
+              <span className="flex" onClick={() => setIsAddModalOpen(true)}>
+                <Button className="flex gap-2 text-white">
                   add user
                   <IoIosAddCircleOutline />
                 </Button>
@@ -94,9 +99,18 @@ const ManageUsers = () => {
         </div>
       </div>
 
-      <AddUserModal isOpen={isAddModalOpen} onClose={() => setIsAddModalOpen(false)} />
-      <EditUserModal isOpen={isEditModalOpen} onClose={() => setIsEditModalOpen(false)} />
-      <DeleteUserModal isOpen={isDeleteModalOpen} onClose={() => setIsDeleteModalOpen(false)} />
+      <AddUserModal
+        isOpen={isAddModalOpen}
+        onClose={() => setIsAddModalOpen(false)}
+      />
+      <EditUserModal
+        isOpen={isEditModalOpen}
+        onClose={() => setIsEditModalOpen(false)}
+      />
+      <DeleteUserModal
+        isOpen={isDeleteModalOpen}
+        onClose={() => setIsDeleteModalOpen(false)}
+      />
     </>
   );
 };
